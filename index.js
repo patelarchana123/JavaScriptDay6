3f4e99769622c3cf52e6c4fb30d24c62ac02a75a
Archana Patel <archana.patel@1Rintern.local>
2023-03-29 15:11:15 +0530

    JavaScript Task

---
+++ index.js
@@ -0,0 +1,70 @@
+// Map method of array
+
+// let arr = [12,3,45];
+// console.log(arr);
+// arr.map((value,index,array)=>console.log(value,index,array));
+// let a = arr.map((value,index,array)=>{
+//     console.log(value,index,array)
+//      return value+index;
+//      });
+// console.log(a);
+
+//Filter method of array
+
+// let arr2 = [1,2,3,4,5,9,90,10,34,56,78];
+// let b = arr2.filter((value)=>{return value>10;});
+// console.log(b);
+
+//Reduce method of array
+// let arr3 = [1,2,34,5,6,7];
+// let newarr3 = arr3.reduce((value1 , value2)=>{
+//      return value1 + value2 ;
+// });
+// console.log(newarr3);
+
+//spread operator 
+// function sum(...Numbers){
+//     let total=0;
+//     for (const iterator of Numbers) {
+//         total += iterator;
+//     }
+//     return total;
+// }
+
+// array = [1,2,3,4,5,6,7,8,9,10];
+// console.log(sum(...array));
+
+let Number = [2,35,46,10,23,34,28,29,30,-1];
+// let number = Number.every((number)=>{
+//     return number>=0;
+// });
+// let number = Number.some((number)=>{
+//     return number>=0;
+// });
+// let number = Number.find((number)=>{return number>10});
+// let number = Number.findLast((number)=>{return number>10});
+// let number = Number.findIndex((number)=>{return number>10});
+// let number = Number.findLastIndex((number)=>{return number>10});
+// let number = Number.filter((number)=>{return number>10});
+// let number = Number.map((number)=>{return number= number*2});
+// let number = Number.reduce((number1,number2)=>{return number1+number2},1);
+// let number = Number.forEach(element => { console.log(element);});
+// let number = Number.sort((a,b)=>{
+//   if (a>b){
+//     return 1;
+//   }if(a<b){
+//     return -1;
+//   }else{
+//     return 0;
+//   }
+
+// });
+// console.log(number);
+
+// let months = [{name:"Jan"},{name:"Feb"},{name:"March"},{name:"july"}];
+// let month = months.filter((value,index)=>({index:value}));
+// console.log(month);
+// console.log(Number);
+// console.log(Number.splice(1));
+// console.log(Number);
+Number.value()
